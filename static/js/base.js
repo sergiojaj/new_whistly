a3a7ee23cf2077e05f6bf8fa2e39599e69d05abb
Sergio Junio <SERGIOA@amdocs.com>
2020-07-17 22:58:20 +0100

    added reply btn to hide/show reply box dynamically

--- static/js/base.js
+++ static/js/base.js
@@ -1,2 +1,12 @@
 console.log('hello there mate')
 
+// function to hide/display reply box on bird_details
+// receives comment id to know under which one to perform action
+function myFunc(reply_id) {
+    var x = document.getElementById(reply_id);
+    if (x.style.display == "none") {
+        x.style.display = 'block';
+    } else {
+        x.style.display = "none";
+    }
+}
