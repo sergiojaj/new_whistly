49cc807aafab90fa90fec15f310b03f85fbd43ed
Sergio Junio <SERGIOA@amdocs.com>
2020-07-17 09:13:09 +0100

    trying to improve on js for comment box

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,2 @@
+console.log('hello there mate')
 
-// this is to display the reply comment div when user click reply
-function showReply() {
-    document.getElementById('reply_box').style.display = "block";
- }
