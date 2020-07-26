5c66c99d58a172206ca8cafd9be87b1b166ea714
Sergio Junio <SERGIOA@amdocs.com>
2020-07-26 12:43:57 +0100

    installed django_heroku app

--- whistly_project/settings.py
+++ whistly_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import logging.config # it has to be .config as logging is just a directory (config is used to pass in dict type config)
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # this var comes from docker-compose.yml
@@ -222,6 +224,7 @@ if ENVIRONMENT == 'production':
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=600)
     DATABASES['default'].update(db_from_env)
+    django_heroku.settings(locals(), logging=False)
 
 # Static files (CSS, JavaScript, Images)
 # Settings for the AWS static files access
@@ -261,11 +264,11 @@ if ENVIRONMENT == 'production':
         },
         'handlers': {
             'null': {
-                'level': 'DEBUG',
+                'level': 'INFO',
                 'class': 'logging.NullHandler',
             },
             'console': {
-                'level': 'DEBUG',
+                'level': 'INFO',
                 'class': 'logging.StreamHandler',
                 'formatter': 'verbose'
             }
--- whistly_project/wsgi.py
+++ whistly_project/wsgi.py
@@ -8,9 +8,44 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import logging.config
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whistly_project.settings')
 
 application = get_wsgi_application()
+
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                        'pathname=%(pathname)s lineno=%(lineno)s ' +
+                        'funcname=%(funcName)s %(message)s'),
+                'datefmt': '%Y-%m-%d %H:%M:%S'
+            },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            }
+        },
+        'handlers': {
+            'null': {
+                'level': 'DEBUG',
+                'class': 'logging.NullHandler',
+            },
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'verbose'
+            }
+        },
+        'loggers': {
+            'testlogger': {
+                'handlers': ['console'],
+                'level': 'INFO',
+            }
+        }
+    }
+logging.config.dictConfig(LOGGING)
