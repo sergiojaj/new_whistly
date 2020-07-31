296cd6863c493210dca0cca9790304c9e4ce9bd9
Sergio Junio <SERGIOA@amdocs.com>
2020-07-31 16:27:24 +0100

    added ajax to add/remove seed

--- static/js/base.js
+++ static/js/base.js
@@ -25,4 +25,77 @@ function myJS(item) {
 function rotate_image(){
 	var img=document.getElementById('bird_image');
 	img.setAttribute('style','transform:rotate(90deg)');
+};
+
+///////////////////////// AJAX ////////////////////////////////
+
+// ajax + jquery to control adding/removing seeds
+$(document).ready(function(){
+    $('#seed').click(function() {
+        $.ajax(
+            {
+                url: "seed",
+                type: "GET",
+                data: {
+                    pk : $('#seed').attr("data-pk")
+                },
+                success: success_func,
+                error: error_func,
+            })
+        })
+    function success_func(data){
+        console.log(data)
+        $('#seed_ico').text(' '+ data['count'])
+    }
+    
+    function error_func(jqXHR, textStatus, errorThrown){
+        console.log(jqXHR)
+        console.log(textStatus)
+        console.log(errorThrown)
+    } 
+})
+
+
+    
+
+
+
+// comment form
+$('#comment_form').on('submit', function(event){
+    event.preventDefault();
+    console.log("form submitted!")
+    var pk;
+    pk = $(this).attr("data-pk");
+    console.log(pk)
+    create_comment()
+});
+
+
+
+function create_comment() {
+    console.log('create comment is working')  // sanity check
+
+    $.ajax(
+        {
+            url : "",
+            type : "POST",
+            data : { 
+                comment_text : $('#comment-text').val(),
+            },
+        // hands a successful response
+            success : function(json) {
+                $('#comment-text').val('');
+                console.log(json);
+                console.log('success');
+        },
+            // handle a error response
+            error : function(xhr,errmsg,err) {
+                $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
+                    " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+                console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+            }
+
+        
+        }
+    )
 }
